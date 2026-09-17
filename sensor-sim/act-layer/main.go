f2d8b0cd0df46db37c133f95d75d8d9038f62ea3
Divij <divijkartikpinge@gmail.com>
2026-09-17 14:15:26 +0530

    Set lower threshold on conflict branch A

--- sensor-sim/act-layer/main.go
+++ sensor-sim/act-layer/main.go
@@ -1,4 +1,5 @@
 package main
+package main
 
 import (
 	"errors"
@@ -7,7 +8,7 @@ import (
 	"strconv"
 )
 
-const temperatureThreshold = 30.0
+const temperatureThreshold = 28.0
 
 type SensorReading struct {
 	DeviceID string
